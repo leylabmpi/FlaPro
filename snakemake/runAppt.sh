f0d449b8004c2b69d45982805a13ed1c3f00f23b
Anna Bogdanova <anna.bogdanova@tuebingen.mpg.de>
2026-05-20 17:03:10 +0200

    switch from singularity to apptainer in running script

--- snakemake/runAppt.sh
+++ snakemake/runAppt.sh
@@ -1,10 +1,10 @@
 # Use the fixed config
 snakemake -f \
     --cores 5 \
-    --use-singularity \
-    --singularity-prefix ./tmp/ \
-    --singularity-args "--bind $(pwd)" \
-    --configfile config_apptainer.yaml \
+    --use-apptainer \
+    --apptainer-prefix ./tmp/ \
+    --apptainer-args "--bind $(pwd)" \
+    --configfile config_apptainer_orig.yaml \
     --printshellcmds \
     --directory $(pwd) \
     --rerun-incomplete
